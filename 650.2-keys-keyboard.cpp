7e4c37d5eaf5c35d28f9f8de0c784bab69aa307b
ZengZheng <51296311+zz9629@users.noreply.github.com>
2025-01-07 17:28:37 +0800

    review

--- 650.2-keys-keyboard.cpp
+++ 650.2-keys-keyboard.cpp
@@ -8,20 +8,6 @@
 class Solution {
 public:
     int minSteps(int n) {
-        // int[] dp = new int[n+1];
-
-        // for (int i = 2; i <= n; i++) {
-        //     dp[i] = i;
-        //     for (int j = i-1; j > 1; j--) {
-        //         if (i % j == 0) {
-        //             dp[i] = dp[j] + (i/j);
-        //             break;
-        //         }
-                
-        //     }
-        // }
-        // return dp[n];
-
         vector<int> dp(n + 1, n);
         dp[0] = dp[1] = 0; // initial with 0 is ok
         for (int i = 2; i <= n; i++)
@@ -31,7 +17,7 @@ public:
             {
                 if(i % j == 0) {    // can be divided by j
                     dp[i] = dp[j] + i/j;
-                    break;
+                    break;          // stop search
                 }
             }
         }
