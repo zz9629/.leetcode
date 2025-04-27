091e35c89647814a14b38e8b4d3458975efb33fe
ZengZheng <51296311+zz9629@users.noreply.github.com>
2025-04-27 21:57:49 +0800

    Mix Leetcode

--- 40.combination-sum-ii.cpp
+++ 40.combination-sum-ii.cpp
@@ -4,11 +4,20 @@
  * [40] Combination Sum II
  */
 
+#include <map>
+#include <vector>
+
+ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    map<int, int> lastIndex;
 
+    /*
+    input [10,1,2,7,6,1,5], 8
+    排序后 [1, 1, 2, 5, 6, 7, 10]
+    Output [[1,1,6],[1,2,5],[1,7],[2,6]]
+    重点是只会生成一个[1,2,5] 而不是两个[1, 2, 5]
+    */
     void backtrace(vector<int>& nums, vector<int>& temp,  vector<vector<int>>& result,  int start, int remain)
     {
         if (remain < 0) return;
@@ -19,9 +28,18 @@ public:
         }
         for (int i = start; i < nums.size(); i++)
         {
+            // 应该是：在同一层for循环时，如果发现重复元素，跳过；而不是在选完元素后跳过。
+            if (i > start && nums[i] == nums[i - 1]) continue;
             temp.push_back(nums[i]);
-            int pos = max(i, lastIndex[nums[i]]);
-            backtrace(nums, temp, result, pos + 1, remain - nums[i]);
+            
+            /*
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i]) {
+                i++;
+            }
+            你在 选完一个数 (temp.push_back(nums[i])) 后立刻跳过了所有重复的数（while (nums[i+1] == nums[i]) i++）。
+            这样做会漏掉一些必须用到重复数的情况，比如 1 + 1 + 6 = 8。
+            */
+            backtrace(nums, temp, result, i + 1, remain - nums[i]);
             temp.pop_back();
         }
         
@@ -29,10 +47,6 @@ public:
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        for (int i = 0; i < candidates.size(); i++)
-        {
-            lastIndex[candidates[i]] = i;
-        }
         vector<vector<int>> res;
         vector<int> temp;
         backtrace(candidates, temp, res, 0, target);
@@ -42,3 +56,12 @@ public:
 };
 // @lc code=end
 
+int main()
+{
+    vector<int> nums = {10,1,2,7,6,1,5};
+    Solution s;
+    auto res = s.combinationSum2(nums, 8);
+
+    return 0;
+
+}
